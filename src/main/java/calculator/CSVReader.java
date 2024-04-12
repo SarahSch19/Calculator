cb7fe03e1f487339f4c94d82f3fc9db40226ea2b
Sarah Schlegel <sschlegel@myges.fr>
2024-04-12 13:38:59 +0200

    read empty line

--- src/main/java/calculator/CSVReader.java
+++ src/main/java/calculator/CSVReader.java
@@ -5,11 +5,13 @@ import java.util.Arrays;
 
 public class CSVReader {
     private final InputStream inputStream;
+    private final BufferedReader reader;
     private String line = "";
 
     public CSVReader(String filename) throws Exception {
         try {
             this.inputStream = new FileInputStream(filename);
+            this.reader = new BufferedReader(new InputStreamReader(this.inputStream));
         } catch (IOException e) {
             throw new Exception("Error opening file " + filename);
         }
@@ -21,14 +23,16 @@ public class CSVReader {
 
     public void readLine() throws Exception {
         try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(this.inputStream));
-            this.line = reader.readLine();
+            String l = reader.readLine();
+            this.line = l != null ? l : "";
+
         } catch (IOException e) {
             throw new RuntimeException("Error reading file");
         }
     }
 
     public int[] parseIntLine() {
+        if (this.line.isEmpty()) return new int[]{};
         return Arrays.stream(this.line.split(",")).map(String::trim).mapToInt(Integer::parseInt).toArray();
     }
 }
